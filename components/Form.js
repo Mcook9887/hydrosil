5348bb821f1bec3460dea7a131336e4c170803fd
Madison <66797979+Mcook9887@users.noreply.github.com>
2024-10-21 08:59:34 -0500

    Update Form.js

--- components/Form.js
+++ components/Form.js
@@ -5,7 +5,7 @@ import TextArea from '../components/form/TextArea'
 function Contact_Us(formJSON) {
     return (
         <div>
-            <form action="https://hydro.server8.turnkeydigital.dev/wp-json/frm/v2/forms/1/entries" method="POST">
+            <form action="https://hydrosilintl.com/wp-json/frm/v2/forms/1/entries" method="POST">
                 {formJSON.formJSON.map((meta, index) => (
                     (
                         meta.type == "text" ? 
